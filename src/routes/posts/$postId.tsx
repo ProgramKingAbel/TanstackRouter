6101c7997d8b42ece0643cbaf358fe99ac889b82
Abel Morara <abelmorara254@gmail.com>
2025-02-17 14:11:14 +0300

    feat: configure loaders and data

--- src/routes/posts/$postId.tsx
+++ src/routes/posts/$postId.tsx
@@ -2,8 +2,19 @@ import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute('/posts/$postId')({
     component: Post,
+    loader: async ({params}) => {
+        // throw new Error();
+        await new Promise((resolve) => setTimeout(resolve, 1000))
+        return {
+            postId: params.postId, 
+        };
+    },
+    pendingComponent: () => <div>Loading ...</div>,
+    errorComponent: () => <div>Error </div>
 })
 
 function Post() {
-    return <div>Hello "/posts/$postId"</div>
+    const { postId } = Route.useLoaderData();
+
+    return <div>Hello {postId} </div>
 }
